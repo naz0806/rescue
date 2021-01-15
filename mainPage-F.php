2dab3d97bcd5b9226288f9db12ac1eaf9926f80b
naz0806 <62138890+naz0806@users.noreply.github.com>
2021-01-15 23:27:55 +0800

    Update mainPage-F.php

--- mainPage-F.php
+++ mainPage-F.php
@@ -6,7 +6,7 @@
     <title>Rescue</title>
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Lato&family=Sacramento&family=Source+Sans+Pro&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lato&family=Sacramento&family=Source+Sans+Pro&display=swap" rel="stylesheet">
     <!-- bootstrap installation/css@bootstrap stylesheet -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <!-- css file -->
