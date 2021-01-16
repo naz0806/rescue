651b7b1a3244879b5c85c19a46fca479c1401978
naz0806 <62138890+naz0806@users.noreply.github.com>
2021-01-16 21:00:29 +0800

    Update register.php

--- register.php
+++ register.php
@@ -4,23 +4,39 @@ require_once('connection.php');
 
 $Fullname = $Email = $Username = $Password = $Contact = '';
 
-$Fullname = $_POST['Fullname'];
-$Email = $_POST['Email'];
-$Username = $_POST['Username'];
-$Password = $_POST['Password'];
-$Contact = $_POST['Contact'];
-$Pass = MD5($Password);
+if (isset($_POST['submit'])) {
+        $Fullname = mysqli_real_escape_string($link, $_POST['Fullname']);
+        $Email = mysqli_real_escape_string($link, $_POST['Email']);
+        $Username = mysqli_real_escape_string($link, $_POST['Username']);
+        $Password = mysqli_real_escape_string($link, $_POST['Password']);
+        $Contact = mysqli_real_escape_string($link, $_POST['Contact']);
+}
 
-$sql = "INSERT INTO users (Fullname, Email, Username, Password, Contact) VALUES ('$Fullname','$Email','$Username','$Pass','$Contact')";
-$result = mysqli_query($conn, $sql);
-if($result)
-{
-	header("Location: https://rescue-cmt322.herokuapp.com/usersSignIn-vol.php");
-}
-else
-{
-	echo "Error :".$sql;
-}
+		
+		
+        $Pass = md5($Password); //encrypt password before storing in database (security)
+        $sql = "INSERT INTO users (Fullname, Email, Username, Password, Contact) VALUES ('$Fullname','$Email','$Username','$Pass','$Contact')";
+        $result = mysqli_query($link, $sql);
+
+	//$result = mysqli_query($conn, $sql);
+	if($result)
+	{
+		header("Location: https://rescue-cmt322.herokuapp.com/usersSignIn-vol.php");
+	}
+	else
+	{
+		echo "Error :".$sql;
+	}
 
 
 ?>
+
+
+
+
+    
+       <!-- $password = md5($d_pass1); //encrypt password before storing in database (security)
+        $sql = "INSERT INTO donor (d_fname, d_lname, d_email, d_pass1, d_pass2)
+                VALUES ('$d_fname', '$d_lname', '$d_email', '$d_pass1', '$d_pass2')";
+        mysqli_query($mysqli, $sql); -->
+    
