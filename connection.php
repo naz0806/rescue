07440744e674901cca60677daaa1f71538930b9c
naz0806 <62138890+naz0806@users.noreply.github.com>
2021-01-16 20:14:21 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,11 +2,22 @@
 in this file we write code for connection with database.
 -->
 <?php
+ //connect to the database
+    $link = mysqli_connect('remotemysql.com', '3Xfb0olTjF', 'kWUZVKJpXB', '3Xfb0olTjF') or die($link);
+
+    if($mysqli->connect_error){
+        die('Connection Failed : '.$mysqli->connect_error);
+    }
+
+?>
+
+
+
+<!--
 $conn = mysqli_connect("remotemysql.com","3Xfb0olTjF","kWUZVKJpXB","3Xfb0olTjF");
 
 if(!$conn)
 {
 	echo "Database connection failed...";
 }
-
-?>
+-->
